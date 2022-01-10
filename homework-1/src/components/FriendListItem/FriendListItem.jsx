1b614948346fbbf054f5b634f35ac0dd9f3e4242
Bevzyk Soi <bevzyk.soi@gmail.com>
2022-01-10 19:47:01 +0200

    Fixed styles for FriendListItem component

--- homework-1/src/components/FriendListItem/FriendListItem.jsx
+++ homework-1/src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import styles from "./FriendListItem.module.css";
 const FriendListItem = ({ isOnline, avatar, name }) => {
     return (
         <li className={styles.item}>
-            <span className={styles.status} id={isOnline ? styles.green : styles.red}></span>
+            <span className={styles.status && isOnline ? styles.green : styles.red}></span>
             <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={styles.name}>{name}</p>
         </li>
